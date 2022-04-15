dc9efa572c9219dcb32fa109f98d2595a4a120c4
Marcin Goncerzewicz <marcingoncerzewicz@gmail.com>
2022-04-16 00:36:42 +0200

    tak to sie zaczelo

--- src/main/service/driver_service.py
+++ src/main/service/driver_service.py
@@ -16,7 +16,7 @@ from service.driver_fee_service import DriverFeeService
 
 def is_base_64(s):
     try:
-        return base64.b64encode(base64.b64decode(s)) == s
+        return base64.standard_b64decode(base64.standard_b64encode(bytes(s.encode("utf8")))) == bytes(s.encode("utf8"))
     except Exception:
         return False
 
